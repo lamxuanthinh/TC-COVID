808e8119466cacf888d2a2b795373ad7622a74c0
phapne <phapne@MacBook-Air-cua-phapne.local>
2022-09-12 19:40:00 +0700

    delete evn

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://tc-covid-json-server.herokuapp.com/doctors",
   headers: {
     "content-type": "application/json",
   },
